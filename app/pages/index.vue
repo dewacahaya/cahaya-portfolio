a3239100fa1a4fc5ad0d19353660acc8d97f0a18
dewacahaya <dewacahaya30@gmail.com>
2026-02-24 10:57:24 +0800

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-surface-light">
+    <p class="text-surface-darker">
+      Ini halaman utama portfolio
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,40 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap' }
+      ]
+    }
+  },
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs',
+        semi: false,
+        quotes: 'single'
+      }
+    }
+  }
+})
